58c8a9a3f78b816c37c4cc55df927105c76294c9
DieMyst <dmitry.shakhtarin@fluence.ai>
2020-11-26 02:46:33 +0300

    interface to string

--- src/ModulePage/View.elm
+++ src/ModulePage/View.elm
@@ -1,8 +1,11 @@
 module ModulePage.View exposing (..)
 
 import Html exposing (Html)
+import Json.Encode as Encode
 import ModulePage.Model exposing (ModuleInfo)
 import Palette exposing (classes)
+import Services.Model exposing (Record)
+import String.Interpolate exposing(interpolate)
 
 view : ModuleInfo -> Html msg
 view moduleInfo =
@@ -15,8 +18,27 @@ view moduleInfo =
 viewInfo : ModuleInfo -> Html msg
 viewInfo moduleInfo =
     Html.article [classes "cf"]
-    [ Html.div [classes "fl w-30 gray"] [Html.text "AUTHOR"]
-    , Html.div [classes "fl w-70"] [ Html.span [classes "fl w-100 black b"] [Html.text moduleInfo.author], Html.span [classes "fl w-100 black"] [Html.text moduleInfo.authorPeerId]]
-    , Html.div [classes "fl w-30 gray"] [Html.text "DESCRIPTION"]
-    , Html.div [classes "fl w-70"] [ Html.span [classes "fl w-100 black b"] [Html.text moduleInfo.author]]
+    [ Html.div [classes "fl w-30 gray mv1"] [Html.text "AUTHOR"]
+    , Html.div [classes "fl w-70 mv1"] [ Html.span [classes "fl w-100 black b"] [Html.text moduleInfo.author], Html.span [classes "fl w-100 black"] [Html.text moduleInfo.authorPeerId]]
+    , Html.div [classes "fl w-30 gray mv1"] [Html.text "DESCRIPTION"]
+    , Html.div [classes "fl w-70 mv1"] [ Html.span [classes "fl w-100 black"] [Html.text moduleInfo.description]]
+    , Html.div [classes "fl w-30 gray mv1"] [Html.text "INTERFACE"]
+    , Html.div [classes "fl w-70 mv1"] [ Html.span [classes "fl w-100 black"] (recordsToString moduleInfo.service.interface.record_types)]
     ]
+
+recordsToString : List Record -> List (Html msg)
+recordsToString record =
+    (List.map recordToString record)
+
+recordToString : Record -> Html msg
+recordToString record =
+    Html.div [classes "i"]
+    ([Html.span [classes "fl w-100 mt2"] [Html.text (record.name ++ " {")]] ++
+    fieldsToString record.fields ++
+    [Html.span [classes "fl w-100 mb2"] [Html.text ("}")]])
+
+
+fieldsToString : List (List String) -> List (Html msg)
+fieldsToString fields =
+    (fields |> List.map (\f -> Html.span [classes "fl w-100 ml2"] [Html.text (String.join ": " f)] ))
+
