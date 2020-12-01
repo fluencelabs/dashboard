427ef2ced79f3a6821f926b3bdcf595edda29072
folex <0xdxdy@gmail.com>
2020-12-01 20:14:02 +0300

    Add faas-net, enable eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,13 @@
 
 import 'tachyons/css/tachyons.min.css';
 import './main.css';
-import {Elm} from './Main.elm';
+import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
-import {peerIdToSeed, seedToPeerId} from "fluence/dist/seed";
+import { peerIdToSeed, seedToPeerId } from "fluence/dist/seed";
 import Fluence from "fluence";
-import {build} from "fluence/dist/particle";
-import {registerService} from "fluence/dist/globalState";
-import {Service, ServiceMultiple, ServiceOne} from "fluence/dist/service";
+import { build } from "fluence/dist/particle";
+import { registerService } from "fluence/dist/globalState";
+import { Service, ServiceMultiple, ServiceOne } from "fluence/dist/service";
 
 let relayIdx = 1
 
@@ -40,7 +40,7 @@ function event(name: string, peer: string, peers?: string[], identify?: string[]
     if (!identify) { identify = null }
     if (!blueprints) { blueprints = null }
 
-    return {name, peer, peers, identify, services, modules, blueprints}
+    return { name, peer, peers, identify, services, modules, blueprints }
 }
 
 (async () => {
@@ -82,13 +82,13 @@ function event(name: string, peer: string, peers?: string[], identify?: string[]
     })
     registerService(eventService)
 
-    app.ports.sendParticle.subscribe(async(part: any) => {
+    app.ports.sendParticle.subscribe(async (part: any) => {
         console.log("Going to build particle", part)
         let jsonData = part.data;
 
         let map = new Map<string, string>()
-        for (let v in jsonData) if(jsonData.hasOwnProperty(v)) {
-            map.set(v,jsonData[v])
+        for (let v in jsonData) if (jsonData.hasOwnProperty(v)) {
+            map.set(v, jsonData[v])
         }
 
         let particle = await build(client.selfPeerId, part.script, map)
@@ -115,7 +115,7 @@ declare global {
 
 window.test = test;
 
-export let relays: { peerId: string; multiaddr: string }[] = [
+export let stage: { peerId: string; multiaddr: string }[] = [
     {
         multiaddr: "/dns4/stage.fluence.dev/tcp/19001/wss/p2p/12D3KooWEXNUbCXooUwHrHBbrmjsrpHXoEphPwbjQXEGyzbqKnE9",
         peerId: "12D3KooWEXNUbCXooUwHrHBbrmjsrpHXoEphPwbjQXEGyzbqKnE9"
@@ -144,4 +144,21 @@ export let relays: { peerId: string; multiaddr: string }[] = [
         multiaddr: "/dns4/stage.fluence.dev/tcp/19100/wss/p2p/12D3KooWPnLxnY71JDxvB3zbjKu9k1BCYNthGZw6iGrLYsR1RnWM",
         peerId: "12D3KooWPnLxnY71JDxvB3zbjKu9k1BCYNthGZw6iGrLYsR1RnWM"
     }
+];
+
+export let faasNet: { peerId: string; multiaddr: string }[] = [
+    { multiaddr: "/ip4/138.68.102.254/tcp/9001/ws/p2p/12D3KooWB9P1xmV3c7ZPpBemovbwCiRRTKd3Kq2jsVPQN4ZukDfy", peerId: "12D3KooWB9P1xmV3c7ZPpBemovbwCiRRTKd3Kq2jsVPQN4ZukDfy" },
+    { multiaddr: "/ip4/165.227.165.192/tcp/9001/ws/p2p/12D3KooWJbJFaZ3k5sNd8DjQgg3aERoKtBAnirEvPV8yp76kEXHB", peerId: "12D3KooWJbJFaZ3k5sNd8DjQgg3aERoKtBAnirEvPV8yp76kEXHB" },
+    { multiaddr: "/ip4/165.227.167.87/tcp/9001/ws/p2p/12D3KooWF7gjXhQ4LaKj6j7ntxsPpGk34psdQicN2KNfBi9bFKXg", peerId: "12D3KooWF7gjXhQ4LaKj6j7ntxsPpGk34psdQicN2KNfBi9bFKXg" },
+    { multiaddr: "/ip4/138.68.70.143/tcp/9001/ws/p2p/12D3KooWKnRcsTpYx9axkJ6d69LPfpPXrkVLe96skuPTAo76LLVH", peerId: "12D3KooWKnRcsTpYx9axkJ6d69LPfpPXrkVLe96skuPTAo76LLVH" },
+    { multiaddr: "/ip4/165.227.161.121/tcp/9001/ws/p2p/12D3KooWBUJifCTgaxAUrcM9JysqCcS4CS8tiYH5hExbdWCAoNwb", peerId: "12D3KooWBUJifCTgaxAUrcM9JysqCcS4CS8tiYH5hExbdWCAoNwb" },
+    { multiaddr: "/ip4/159.89.12.142/tcp/9001/ws/p2p/12D3KooWCKCeqLPSgMnDjyFsJuWqREDtKNHx1JEBiwaMXhCLNTRb", peerId: "12D3KooWCKCeqLPSgMnDjyFsJuWqREDtKNHx1JEBiwaMXhCLNTRb" },
+    { multiaddr: "/ip4/139.59.146.127/tcp/9001/ws/p2p/12D3KooWEXNUbCXooUwHrHBbrmjsrpHXoEphPwbjQXEGyzbqKnE9", peerId: "12D3KooWEXNUbCXooUwHrHBbrmjsrpHXoEphPwbjQXEGyzbqKnE9" },
+    { multiaddr: "/ip4/159.89.9.74/tcp/9001/ws/p2p/12D3KooWHk9BjDQBUqnavciRPhAYFvqKBe4ZiPPvde7vDaqgn5er", peerId: "12D3KooWHk9BjDQBUqnavciRPhAYFvqKBe4ZiPPvde7vDaqgn5er" },
+    { multiaddr: "/ip4/159.89.6.161/tcp/9001/ws/p2p/12D3KooWGzNvhSDsgFoHwpWHAyPf1kcTYCGeRBPfznL8J6qdyu2H", peerId: "12D3KooWGzNvhSDsgFoHwpWHAyPf1kcTYCGeRBPfznL8J6qdyu2H" },
+    { multiaddr: "/ip4/165.227.163.146/tcp/9001/ws/p2p/12D3KooWBSdm6TkqnEFrgBuSkpVE3dR1kr6952DsWQRNwJZjFZBv", peerId: "12D3KooWBSdm6TkqnEFrgBuSkpVE3dR1kr6952DsWQRNwJZjFZBv" },
+    { multiaddr: "/ip4/139.59.146.127/tcp/9990/ws/p2p/12D3KooWMhVpgfQxBLkQkJed8VFNvgN4iE6MD7xCybb1ZYWW2Gtz", peerId: "12D3KooWMhVpgfQxBLkQkJed8VFNvgN4iE6MD7xCybb1ZYWW2Gtz" },
+    { multiaddr: "/ip4/139.59.146.127/tcp/9100/ws/p2p/12D3KooWPnLxnY71JDxvB3zbjKu9k1BCYNthGZw6iGrLYsR1RnWM", peerId: "12D3KooWPnLxnY71JDxvB3zbjKu9k1BCYNthGZw6iGrLYsR1RnWM" }
 ]
+
+export let relays: { peerId: string; multiaddr: string }[] = stage;
