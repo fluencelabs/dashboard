b61b44de58d0b09e149cbafbad7515f1b93a22e9
DieMyst <dmitry.shakhtarin@fluence.ai>
2020-12-10 17:32:25 +0300

    update

--- src/Info.elm
+++ src/Info.elm
@@ -33,7 +33,7 @@ modulesDescription =
         , ( "sqlite", { site = "https://github.com/fluencelabs/sqlite", description = "Popular embeddable database compiled to WebAssembly, stores data in memory" } )
         , ( "history", { site = "https://github.com/fluencelabs/aqua-demo/tree/master/services/history", description = "Stores message log, used in History service" } )
         , ( "userlist", { site = "https://github.com/fluencelabs/aqua-demo/tree/master/services/user-list", description = "Address book implementation module, used in User List service" } )
-        , ( "redis", { site = "https://github.com/fluencelabs/redis", description = "Popular embeddable database compiled to WebAssembly, stores data in memory" } )
+        , ( "redis", { site = "https://github.com/fluencelabs/redis", description = "Popular NoSQL K/V storage compiled to WebAssembly, stores data in memory" } )
         , ( "curl", { site = "https://github.com/fluencelabs/fce/tree/master/examples/url-downloader/curl", description = "Adapter module for cURL CLI utility" } )
         , ( "local_storage", { site = "https://github.com/fluencelabs/fce/tree/master/examples/url-downloader/local_storage", description = "Provides methods for working with file system: put and get" } )
         , ( "curl_adapter", { site = "https://github.com/fluencelabs/fce/tree/master/examples/url-downloader/facade", description = "Adapter module for cURL CLI utility" } )
@@ -47,6 +47,6 @@ blueprintsDescription =
         [ ( "SQLite 3", { description = "Popular embeddable database compiled to WebAssembly" } )
         , ( "Message History", { description = "Stores message log, used in the Chat application" } )
         , ( "User List", { description = "Basically an address book. Used in the Chat application to store chat users" } )
-        , ( "Redis", { description = "Popular embeddable database compiled to WebAssembly" } )
+        , ( "Redis", { description = "Popular NoSQL K/V storage compiled to WebAssembly, stores data in memory" } )
         , ( "URL Downloader", { description = "cURL adapter, allows to download anything by URL" } )
         ]
