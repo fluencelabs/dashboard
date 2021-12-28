b5d8256e10130e22a1cca11ea4fd85a23acb6946
Pavel Murygin <Pavel.Murygin@gmail.com>
2021-12-28 20:24:31 +0300

    Update fluence-js to the version version with avm runner

--- webpack.config.js
+++ webpack.config.js
@@ -129,7 +129,7 @@ if (MODE === 'development') {
         devServer: {
             inline: true,
             stats: 'errors-only',
-            contentBase: path.join(__dirname, 'src/assets'),
+            contentBase: path.join(__dirname, 'public'),
             historyApiFallback: true,
             // feel free to delete this section if you don't need anything like this
             before(app) {
