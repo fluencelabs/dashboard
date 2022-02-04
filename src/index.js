1552814ebcd230a77601b076552da87d7dd0104f
folex <0xdxdy@gmail.com>
2022-02-04 14:58:03 +0300

    Add 'showStat', update fluenceJS & aqua

--- src/index.js
+++ src/index.js
@@ -127,6 +127,118 @@ function genFlags(peerId, relays, relayIdx) {
         flags: flags,
     });
 
+    if (window) {
+        window.collectedData = {
+            // service ids
+            services: new Map(),
+            // services per creator
+            creators: new Map(),
+            // blueprints used in services
+            serviceBlueprints: new Set(),
+            // all blueprints
+            blueprints: new Map(),
+            // module hashes used in services
+            serviceModules: new Set(),
+            // unique module names used in services
+            moduleNames: new Set(),
+            // all modules
+            modules: new Map(),
+            // peer infos per PeerId
+            peerInfos: new Map(),
+        };
+
+        window.showStat = () => {
+            /*
+                1490 users created 3018 services
+                from 280 blueprints
+                from 299 modules of which there are 43 unique module names
+            */
+            let usersCount = window.collectedData.creators.size;
+            let serviceCount = window.collectedData.services.size;
+            let blueprintCount = window.collectedData.serviceBlueprints.size;
+            let moduleCount = window.collectedData.serviceModules.size;
+            let nameCount = window.collectedData.moduleNames.size;
+            let totalModules = window.collectedData.modules.size;
+
+            console.log(`${usersCount} users created ${serviceCount} services`);
+            console.log(`from ${blueprintCount} blueprints`);
+            console.log(`from ${moduleCount} modules of which there are ${nameCount} unique module names`);
+            console.log(`total ${totalModules} modules were uploaded`);
+
+            let servicesPerHost = new Map();
+            for (let service of window.collectedData.services.values()) {
+                let perHost = servicesPerHost.has(service.host) ? servicesPerHost.get(service.host) : new Set();
+                perHost.add(service.id);
+                servicesPerHost.set(service.host, perHost);
+            }
+
+            let N = 10;
+            let sorted = [...servicesPerHost.entries()].sort((a, b) => b[1].size - a[1].size);
+            console.log(`top ${N} nodes by hosted services:`)
+            let slice = sorted.slice(0, 10);
+            for (let entry of slice) {
+                console.log(`\t node ${entry[0]} has ${entry[1].size} services`);
+            }
+
+            let byNodeVersion = new Map();
+            let byAirVersion = new Map();
+            for (let info of window.collectedData.peerInfos.values()) {
+                let byNodeCount = byNodeVersion.get(info.node_version) || 0;
+                byNodeVersion.set(info.node_version, byNodeCount + 1)
+                let byAirCount = byAirVersion.get(info.air_version) || 0;
+                byAirVersion.set(info.air_version, byAirCount + 1)
+            }
+
+            console.log("Nodes version distribution:");
+            for (let entry of byNodeVersion.entries()) {
+                let version = entry[0];
+                let count = entry[1];
+                console.log(`\t${version}: ${count} nodes`);
+            }
+
+            console.log("AIR intepreter versions distribution:");
+            for (let entry of byAirVersion.entries()) {
+                let version = entry[0];
+                let count = entry[1];
+                console.log(`\t${version}: ${count} nodes`);
+            }
+        };
+    }
+
+    function collectStats(peerId, identify, services, blueprints, modules) {
+        if (window) {
+            window.collectedData.peerInfos.set(peerId, identify);
+
+            for (let blueprint of blueprints) {
+                window.collectedData.blueprints.set(blueprint.id, blueprint);
+            }
+
+            for (let module of modules) {
+                window.collectedData.modules.set(module.hash, module);
+            }
+
+            for (let service of services) {
+                service.host = peerId;
+                window.collectedData.services.set(service.id, service);
+                window.collectedData.serviceBlueprints.add(service.blueprint_id);
+
+                let blueprint = window.collectedData.blueprints.get(service.blueprint_id);
+                for (let prefixedHash of blueprint.dependencies) {
+                    let moduleHash = prefixedHash.split(':')[1];
+                    window.collectedData.serviceModules.add(moduleHash);
+
+                    let module = window.collectedData.modules.get(moduleHash);
+                    window.collectedData.moduleNames.add(module.name);
+                }
+
+                let perCreator = window.collectedData.creators.has(service.owner_id) ? window.collectedData.creators.get(service.owner_id) : new Set();
+                perCreator.add(service.id);
+                window.collectedData.creators.set(service.owner_id, perCreator);
+            }
+
+        }
+    }
+
     // alias ServiceInterfaceCb: PeerId, string, Interface -> ()
     function collectServiceInterface(peer_id, service_id, iface) {
         // console.count(`service interface from ${peer_id}`);
@@ -146,7 +258,9 @@ function genFlags(peerId, relays, relayIdx) {
     // alias PeerInfoCb: PeerId, Info, []Service, []Blueprint, []Module -> ()
     function collectPeerInfo(peerId, identify, services, blueprints, modules, interfaces) {
         // console.log('peer info from %s, %s services', peerId, services.length);
-        try {            
+        try {
+            collectStats(peerId, identify, services, blueprints, modules);
+
             const eventRaw = {
                 // HACK: show AIR interpreter version in the NODE ID field
                 peerId: peerId + ' (' + identify.air_version + ')',
